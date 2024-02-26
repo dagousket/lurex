7fe6a3a2b3376c045fbbdaf8b92114766734cb44
dagousket <swann.flochlay@gmail.com>
2024-02-26 10:56:29 +0100

    feat: some more UI

--- R/mod_combine_brick.R
+++ R/mod_combine_brick.R
@@ -8,44 +8,75 @@
 #'
 #' @importFrom shiny NS tagList
 #' @importFrom sortable bucket_list add_rank_list
+#' @importFrom htmltools tags
+#' @importFrom bslib tooltip
+#' @importFrom bsicons bs_icon
 mod_combine_brick_ui <- function(id) {
-	ns <- NS(id)
-	tagList(
-		h3("Order your motifs"),
-		bucket_list(
-			header = "plop",
-			group_name = "brick_bucket",
-			orientation = "vertical",
-			add_rank_list(
-				input_id = "ranked_bricks",
-				text = "",
-				labels = c("[mot1]", "[mot2]", "[mot3]", "[mot4]"),
-				orientation = "horizontal"
-			)
-		),
-		fluidRow(
-			column(
-				4,
-				bucket_list(
-					header = "bin",
-					group_name = "brick_bucket",
-					add_rank_list(
-						input_id = "dumped_bricks",
-						text = ""
-					)
-				)
-			)
-		)
-	)
+  ns <- NS(id)
+  tagList(
+    h3("Order your motifs"),
+    bucket_list(
+      header = "plop",
+      group_name = "brick_bucket",
+      orientation = "vertical",
+      add_rank_list(
+        input_id = "ranked_bricks",
+        text = "",
+        labels = c("[mot1]", "[mot2]", "[mot3]", "[mot4]"),
+        orientation = "horizontal"
+      )
+    ),
+    fluidRow(
+      column(
+        4,
+        bucket_list(
+          header = "bin",
+          group_name = "brick_bucket",
+          add_rank_list(
+            input_id = "dumped_bricks",
+            text = ""
+          )
+        )
+      ),
+      column(
+        8,
+        bucket_list(
+          header = "helpers",
+          group_name = "brick_bucket",
+          add_rank_list(
+            input_id = "helper_bricks",
+            text = "",
+            labels = list(
+              "or" = tags$div(
+                "|", tooltip(bs_icon("info-circle"), "or : match the motif on the left or the motif on the right", placement = "bottom")
+              ),
+              "start" = tags$div(
+                "^", tooltip(bs_icon("info-circle"), "start : no text before this", placement = "bottom")
+              ),
+              "end" = tags$div(
+                "$", tooltip(bs_icon("info-circle"), "stop : no text after this", placement = "bottom")
+              )
+            ),
+            orientation = "horizontal"
+          )
+        )
+      )
+    ),
+    actionButton(
+      inputId = "clear_bin",
+      label = "empty bin",
+      icon = icon("trash")
+    )
+  )
 }
 
 #' combine_brick Server Functions
 #'
 #' @noRd
 mod_combine_brick_server <- function(id) {
-	moduleServer(id, function(input, output, session) {
-		ns <- session$ns
-	})
+  moduleServer(id, function(input, output, session) {
+    ns <- session$ns
+  })
 }
 
 ## To be copied in the UI
--- R/mod_regex_brick.R
+++ R/mod_regex_brick.R
@@ -8,40 +8,91 @@
 #'
 #' @importFrom shiny NS tagList
 #' @importFrom shinyWidgets pickerInput
+#' @importFrom bslib tooltip
+#' @importFrom bsicons bs_icon
 mod_regex_brick_ui <- function(id) {
-	ns <- NS(id)
-	tagList(
-		h3("Construct a regex brick"),
-		pickerInput(
-			inputId = "motif_type",
-			label = "Type of motif",
-			choices = c("unordered", "ordered")
-		),
-		selectInput(
-			inputId = "content_type",
-			label = "Type of content",
-			choices = c("character", "digits", "punctuations")
-		),
-		textInput(
-			inputId = "custom_text",
-			label = "custom motif",
-			value = "write your motif here"
-		),
-		actionButton(
-			inputId = "make_brick",
-			label = "create",
-			icon = icon("circle-plus")
-		)
-	)
+  ns <- NS(id)
+  tagList(
+    h3("Construct a regex brick"),
+    p(),
+    selectInput(
+      inputId = "motif_type",
+      label = span(
+        "type of text",
+        tooltip(
+          bs_icon("info-circle"),
+          "some info here",
+          placement = "bottom"
+        )
+      ),
+      choices = c("unordered", "ordered")
+    ),
+    p(),
+    selectInput(
+      inputId = "content_type",
+      label = span(
+        "type of content",
+        tooltip(
+          bs_icon("info-circle"),
+          "some info here",
+          placement = "bottom"
+        )
+      ),
+      choices = c("wildcard", "character", "digits", "punctuations", "custom")
+    ),
+    p(),
+    textInput(
+      inputId = "custom_text",
+      label = span(
+        "custom motif",
+        tooltip(
+          bs_icon("info-circle"),
+          "some info here",
+          placement = "bottom"
+        )
+      ),
+      value = "write your motif here"
+    ),
+    p(),
+    selectInput(
+      inputId = "occurrence",
+      label = span(
+        "occurence",
+        tooltip(
+          bs_icon("info-circle"),
+          "some info here",
+          placement = "bottom"
+        )
+      ),
+      choices = c("once", "at least once", "anytime", "custom range")
+    ),
+    p(),
+    sliderInput(
+      inputId = "occurence_slider",
+      label = "custom range",
+      min = 0,
+      max = 10,
+      value = c(1, 2),
+      ticks = FALSE,
+      step = 1,
+      round = TRUE
+    ),
+    p(),
+    actionButton(
+      inputId = "make_brick",
+      label = "create",
+      icon = icon("circle-plus")
+    )
+  )
 }
 
 #' regex_brick Server Functions
 #'
 #' @noRd
 mod_regex_brick_server <- function(id) {
-	moduleServer(id, function(input, output, session) {
-		ns <- session$ns
-	})
+  moduleServer(id, function(input, output, session) {
+    ns <- session$ns
+  })
 }
 
 ## To be copied in the UI
